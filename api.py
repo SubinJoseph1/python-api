4c59690f4d00e1dd90fd428c4aa0d87191fd12b7
SubinJ <subin.j@idp.com>
2024-07-02 10:20:31 +0530

    second commit

--- api.py
+++ api.py
@@ -1,2 +1,10 @@
 # Github project url
 # https://github.com/SubinJoseph1/python-api.git
+# git deploy comments
+# git init
+# git add README.md
+# git add api.py
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/SubinJoseph1/python-api.git
+# git push -u origin main
